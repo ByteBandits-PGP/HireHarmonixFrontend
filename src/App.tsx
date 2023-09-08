6c3ca6f2fb89fae2adff82ffe715b8a70f423cf9
Ayşe Aker Tütüncü <akerayse@gmail.com>
2023-09-08 20:01:19 +0200

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css'
 import SalesPersonPage from './components/salesPersonPageComponents/SalesPersonPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/commonComponents/NavBar"
 import { Routes, Route } from "react-router-dom";
+import Landing from './components/Landing';
+import Login from './components/Login';
 
 function App() {
-
   return (
     <>
-      <NavBar/>
-      {/* <SalesPersonPage/> */}
       <Routes>
-        <Route path="/manageUsers" element={<SalesPersonPage/>}></Route>
+        <Route path="/" element={<Landing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/manageUsers" element={<SalesPersonPage />}></Route>
       </Routes>
     </>
   )
