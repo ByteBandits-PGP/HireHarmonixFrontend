743b10524dce2e401e25150c849fb2012bce801a
Vijani <vijani@ravindu-Inspiron-3542.home>
2023-09-08 09:49:37 +0200

    Sales person page partially completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import SalesPersonPage from './components/salesPersonPageComponents/SalesPersonPage'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/commonComponents/NavBar"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      
+      <NavBar/>
+      <SalesPersonPage/>
+      {/* <Routes>
+        <Route path="/salesPersonPage" element={<SalesPersonPage/>}></Route>
+      </Routes> */}
     </>
   )
 }
