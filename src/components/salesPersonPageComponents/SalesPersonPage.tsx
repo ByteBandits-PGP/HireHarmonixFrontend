745c4a902eb5151c81c861c8ee988ea0727b39e5
Vijani <vijani@ravindu-Inspiron-3542.home>
2023-09-15 13:50:55 +0200

    some style on salesperson page, navigation on landing page

--- src/components/salesPersonPageComponents/SalesPersonPage.tsx
+++ src/components/salesPersonPageComponents/SalesPersonPage.tsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { ApplicantDetails } from './ApplicantDetails';
+import './SalesPersonPage.css'
 
 
-export const SalesPersonPage = () => {
+const SalesPersonPage = () => {
   const [defaultItem, setDefaultItem] = useState("Applicants");
 
   const handleItemClick = (item: any) => {
@@ -14,25 +15,28 @@ export const SalesPersonPage = () => {
   return (
     <>
       <div className="dropdown-div">
-
+        
         <div className="dropdown-div_div">
-          <label>Select User Type: </label></div>
+        <label>Select User Type: </label>
+          </div>
 
         <div className="dropdown-div_div">
           <DropdownButton
             id="dropdown-basic-button"
             title={defaultItem}
             variant="Secondary"
+            className='doropdown-sales'
           >
             <Dropdown.Item onClick={() => handleItemClick("Applicants")}>Applicants</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleItemClick("Bootcampers")}>Bootcampers</Dropdown.Item>
-            <Dropdown.Item onClick={() => handleItemClick("PGP Salties")}>PGP Salties</Dropdown.Item>
+            <Dropdown.Item onClick={() => handleItemClick("Students")}>Students</Dropdown.Item>
+            <Dropdown.Item onClick={() => handleItemClick("Graduates")}>Graduates</Dropdown.Item>
           </DropdownButton></div>
 
       </div>
+      
       {defaultItem === "Applicants" && <ApplicantDetails />}
-      {defaultItem === "Bootcampers" && <h1>Bootcampers</h1>}
-      {defaultItem === "PGP Salties" && <h1>PGP Salties</h1>}
+      {defaultItem === "Students" && <h1>Students</h1>}
+      {defaultItem === "Graduates" && <h1>Graduates</h1>}
 
     </>
 
